87a4ce8299687531037007da0002b8457a9be570
Diana Ferradini <ferradinid@gmail.com>
2026-09-15 10:31:59 +0000

    feat: ajouter les enums du domaine

---
+++ src/main/java/com/ism/domain/StatutReservation.java
@@ -0,0 +1,8 @@
+package com.ism.domain;
+
+public enum StatutReservation {
+    EN_ATTENTE,
+    EN_COURS,
+    ANNULE,
+    TERMINE;
+}
---
+++ src/main/java/com/ism/domain/TypeSalle.java
@@ -0,0 +1,9 @@
+package com.ism.domain;
+
+public enum TypeSalle {
+    COURS,
+    SOUTENANCES, 
+    REUNIONS,
+    TRAVAUX_PRATIQUES,
+    EVENEMENTS_ETUDIANTS;
+}
