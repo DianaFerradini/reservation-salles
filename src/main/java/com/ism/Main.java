f31706e4df65ab88dfb423a4f6225a669dfc0ac7
Diana Ferradini <ferradinid@gmail.com>
2026-09-15 12:00:41 +0000

    feat: ajouter les salles de demonstration

--- src/main/java/com/ism/Main.java
+++ src/main/java/com/ism/Main.java
@@ -1,7 +1,21 @@
 package com.ism;
 
+import java.util.ArrayList;
+
+import com.ism.domain.Reservation;
+import com.ism.domain.Salle;
+import com.ism.services.Initialisation;
+import com.ism.services.ReservationService;
+import com.ism.services.SalleService;
+import com.ism.views.UserView;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Initialisation.initSalles();
+        Initialisation.initReservations();
+        ArrayList<Salle> salles = SalleService.getAll();
+        ArrayList<Reservation> reservations = ReservationService.getAll();
+        UserView.AfficherSalles(salles);
+        UserView.AfficherReservations(reservations);
     }
 }
